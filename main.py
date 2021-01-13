0c1cf0718f3f4c053da4f4b7ce40d32047ddfe05
atrusty <trusty.aaron.m@gmail.com>
2021-01-13 16:43:20 -0600

    ydk file format, creates random deck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import random
 
 #api stuffs
 YGO_API_ENDPOINT = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
@@ -9,10 +10,10 @@ YGO_PHOTOCLOUD_ENDPOINT = 'https://storage.googleapis.com/ygoprodeck.com/pics/'
 IOC_URI = 'invasion%20of%20chaos'
 
 #ydk file format
-RANDOMIZER_TAG = '#created by db-randomizer'
-MAIN_DECK = '#main'
-EXTRA_DECK = '#extra'
-SIDE_DECK = '!side'
+RANDOMIZER_TAG = '#created by db-randomizer\n'
+MAIN_DECK = '#main\n'
+EXTRA_DECK = '#extra\n'
+SIDE_DECK = '!side\n'
 
 with open('banlist.json') as f:
     banlist = json.load(f)
@@ -20,7 +21,14 @@ with open('banlist.json') as f:
 #print(banlist["18144506"]) #test some random banned card (harpie's feather duster)
 response = requests.get(YGO_API_ENDPOINT + CARDSET_QUERY + IOC_URI)
 cardSet = response.json()["data"]
+deck = []
 
-for card in cardSet:
-    print(str(card["name"]) + " " + str(card["id"]))
-    print('\n')
+f = open("outputdeck.ydk", "a")
+f.write(RANDOMIZER_TAG)
+f.write(MAIN_DECK)
+for iteration in range(40):
+    f.write(str(random.choice(cardSet)["id"]) + '\n')
+f.write(EXTRA_DECK)
+f.write(SIDE_DECK)
+f.close()
+
