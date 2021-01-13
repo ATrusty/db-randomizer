9d53c3690353f1113c5cf50b73606ccf5454cb49
atrusty <trusty.aaron.m@gmail.com>
2021-01-13 01:31:27 -0600

    banlist json and associated ydk file groups

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import json
+
+#api stuffs
 YGO_API_ENDPOINT = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
 ID_QUERY = '?id='
 YGO_PHOTOCLOUD_ENDPOINT = 'https://storage.googleapis.com/ygoprodeck.com/pics/'
-print('hello world')
+
+#ydk file format
+RANDOMIZER_TAG = '#created by db-randomizer'
+MAIN_DECK = '#main'
+EXTRA_DECK = '#extra'
+SIDE_DECK = '!side'
+
+import json
+
+with open('banlist.json') as f:
+    banlist = json.load(f)
+
+print(banlist["18144506"]) #test some random banned card (harpie's feather duster)
