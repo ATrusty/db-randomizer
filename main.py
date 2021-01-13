674258f1f99e55ad266c345ef240c8aee791c9bd
atrusty <trusty.aaron.m@gmail.com>
2021-01-13 00:35:39 -0600

    rename photo endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 YGO_API_ENDPOINT = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
 ID_QUERY = '?id='
-YGO_GC_ENDPOINT = 'https://storage.googleapis.com/ygoprodeck.com/pics/'
+YGO_PHOTOCLOUD_ENDPOINT = 'https://storage.googleapis.com/ygoprodeck.com/pics/'
 print('hello world')
