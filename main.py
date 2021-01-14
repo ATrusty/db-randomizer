74f7615edbd8f0cffb81016187bfe4023cb91b71
atrusty <trusty.aaron.m@gmail.com>
2021-01-14 00:21:24 -0600

    obeys banlist

--- main.py
+++ main.py
@@ -16,8 +16,13 @@ MAIN_DECK = '#main\n'
 EXTRA_DECK = '#extra\n'
 SIDE_DECK = '!side\n'
 
-with open('banlist.json') as f:
-    banlist = json.load(f)
+#while conditions
+MD_ITERS = 40 #command arg?
+SD_ITERS = 15
+ED_ITERS = 15
+
+with open('banlist.json') as banFile:
+    banlist = json.load(banFile)
 
 response = requests.get(YGO_API_ENDPOINT + DATE_QUERY)
 cardSet = response.json()["data"]
@@ -29,16 +34,51 @@ f = open("outputdeck.ydk", "w")
 f.write(RANDOMIZER_TAG)
 #let's generate the main deck
 f.write(MAIN_DECK)
-for iteration in range(40):
-    f.write(str(random.choice(deckCandidates)["id"]) + '\n')
+while(MD_ITERS > -1):
+    currentCardID = str(random.choice(deckCandidates)["id"])
+    if(not currentCardID in banlist.keys()):
+        f.write(currentCardID + '\n')
+        MD_ITERS-=1
+    else:
+        if(banlist[currentCardID] > 0):
+            banlist[currentCardID] = banlist[currentCardID]-1
+            f.write(currentCardID + '\n')
+            MD_ITERS-=1
+        else:
+            pass
 #let's generate the extra deck
 f.write(EXTRA_DECK)
-for iteration in range(15):
-    f.write(str(random.choice(extraDeckCandidates)["id"]) + '\n')
+while(ED_ITERS > -1):
+    currentCardID = str(random.choice(extraDeckCandidates)["id"])
+    if(not currentCardID in banlist.keys()):
+        f.write(currentCardID + '\n')
+        ED_ITERS-=1
+    else:
+        if(banlist[currentCardID] > 0):
+            banlist[currentCardID] = banlist[currentCardID]-1
+            f.write(currentCardID + '\n')
+            ED_ITERS-=1
+        else:
+            pass
+
+# for iteration in range(15):
+#     f.write(str(random.choice(extraDeckCandidates)["id"]) + '\n')
 #let's generate the side deck
 f.write(SIDE_DECK)
-for iteration in range(15):
-    f.write(str(random.choice(deckCandidates)["id"]) + '\n')
+while(SD_ITERS > -1):
+    currentCardID = str(random.choice(deckCandidates)["id"])
+    if(not currentCardID in banlist.keys()):
+        f.write(currentCardID + '\n')
+        SD_ITERS-=1
+    else:
+        if(banlist[currentCardID] > 0):
+            banlist[currentCardID] = banlist[currentCardID]-1
+            f.write(currentCardID + '\n')
+            SD_ITERS-=1
+        else:
+            pass
+# for iteration in range(15):
+#     f.write(str(random.choice(deckCandidates)["id"]) + '\n')
 
 #done
 f.close()
